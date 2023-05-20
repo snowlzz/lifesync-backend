7a5a4fae8302585587b0090e9e5bfd8fcef5c3f6
“Gabriel <gabrielneves.dev@gmail.com>
2023-05-20 18:09:22 -0300

    hashed pass

---
+++ src/user/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { hash } from 'bcrypt';
+import { createUserDto } from './dtos/createUser.dto';
+import { UserEntity } from './entitys/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>
+    ) {}
+
+
+
+
+
+
+    async createUser(createUserDto: createUserDto): Promise<UserEntity> {
+        const saltOrRounds = 10;
+
+        const hashedPassword = await hash(createUserDto.password, saltOrRounds)
+
+        return this.userRepository.save({
+            ... createUserDto,
+            password: hashedPassword
+        });
+
+    }
+    
+    async getAllUsers(): Promise<UserEntity[]> {
+        return this.userRepository.find();
+    }
+}
