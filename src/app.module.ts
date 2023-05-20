4c0e6e31171d806b563f5e017692adaf7028cd8a
“Gabriel <gabrielneves.dev@gmail.com>
2023-05-20 18:11:26 -0300

    postgres on

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user/entitys/user.entity';
+
 
 @Module({
+
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.development.env',
+      envFilePath: '.env.development.local'
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
+      port: parseInt(process.env.DB_PORT),
+      entities: [UserEntity],
+      
     }),
     UserModule,
+
   ],
   controllers: [AppController],
   providers: [AppService],
