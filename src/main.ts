0dcd71ab1bae89fc13669c6246b4fffc5897628e
“Gabriel <gabrielneves.dev@gmail.com>
2023-05-20 18:13:05 -0300

    8080

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { UserEntity } from './user/entitys/user.entity';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8080);
+  
 }
 bootstrap();
