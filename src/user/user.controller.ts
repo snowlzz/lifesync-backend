9be28dd02258f85d19116dd9fb9e50b00b6240f1
“Gabriel <gabrielneves.dev@gmail.com>
2023-05-20 18:08:51 -0300

    add get method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,20 +1,24 @@
-import { Body, Controller, Get , Post} from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { createUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { UserEntity } from './entitys/user.entity';
 
 
 
 
 @Controller('user')
 export class UserController {
-   @Post()
-   async createUser(
-    @Body() createUser: createUserDto,
-   ){
-    return {
-        ...createUser,
-        password: undefined,
+
+    constructor(private readonly userService: UserService) { }
+    @Post()
+    async createUser(@Body() createUser: createUserDto): Promise<UserEntity> {
+        return this.userService.createUser(createUser);
     }
-   }
+
+    @Get()
+    async getAllUsers(): Promise<UserEntity[]> {
+        return this.userService.getAllUsers();
+    }
 
 
 }
